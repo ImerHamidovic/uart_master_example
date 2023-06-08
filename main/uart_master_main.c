48e073bc2dfff259b88d1805cf8c8d33849f4ea2
Imer Hamidovic <imerhama@gmail.com>
2023-06-08 11:55:36 +0200

    Inital commit

---
+++ main/uart_master_main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
+#include "esp_log.h"
+
+#define TAG "MASTER"
+
+/**
+ * This is an example which echos any data it receives on UART1 back to the sender,
+ * with hardware flow control turned off. It does not use UART driver event queue.
+ *
+ * - Port: UART1
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below
+ */
+
+#define ECHO_TEST_TXD  (GPIO_NUM_16)
+#define ECHO_TEST_RXD  (GPIO_NUM_17)
+#define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
+
+#define BUF_SIZE (1024)
+
+static void echo_task(void *arg)
+{
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config =
+    {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    uart_driver_install(UART_NUM_2, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_2, &uart_config);
+    uart_set_pin(UART_NUM_2, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
+
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data_from_master = (uint8_t *) malloc(BUF_SIZE);
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+
+    char string[BUF_SIZE]= "Hello world from master!";
+    memcpy(data_from_master, string, BUF_SIZE);
+
+    while (1)
+    {
+        // Write data back to the UART
+        uart_write_bytes(UART_NUM_2, (const char *) data_from_master, BUF_SIZE);
+
+        // Read data from the UART
+        uart_read_bytes(UART_NUM_2, data, BUF_SIZE, 200 / portTICK_RATE_MS);
+        ESP_LOGI(TAG, "Received from SLAVE: %s", data);
+
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void)
+{
+    xTaskCreate(echo_task, "UART_MASTER_TASK", 5*1024, NULL, 10, NULL);
+}
