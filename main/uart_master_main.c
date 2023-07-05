348d5f8b106f536c497d92500ec81daa121f4522
Imer Hamidovic <i.hamidovic@reply.de>
2023-07-05 14:55:13 +0200

    Update the pin numbers

--- main/uart_master_main.c
+++ main/uart_master_main.c
@@ -18,12 +18,13 @@
  * - Pin assignment: see defines below
  */
 
-#define PIN_TXD  (33)
-#define PIN_RXD  (39)
+#define ECHO_TEST_TXD  (GPIO_NUM_16)
+#define ECHO_TEST_RXD  (GPIO_NUM_17)
 #define ECHO_TEST_RTS  (UART_PIN_NO_CHANGE)
 #define ECHO_TEST_CTS  (UART_PIN_NO_CHANGE)
 
 #define BUF_SIZE (1024)
+#define WRITE_FROM_TERMINAL
 
 static void master_read_write_task(void *arg)
 {
@@ -40,7 +41,7 @@ static void master_read_write_task(void *arg)
     };
     uart_driver_install(UART_NUM_2, BUF_SIZE * 2, 0, 0, NULL, 0);
     uart_param_config(UART_NUM_2, &uart_config);
-    uart_set_pin(UART_NUM_2, PIN_TXD, PIN_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
+    uart_set_pin(UART_NUM_2, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
 
     // Configure a temporary buffer for the incoming data
     uint8_t *data_from_master = (uint8_t *) malloc(BUF_SIZE);
@@ -57,6 +58,7 @@ static void master_read_write_task(void *arg)
         {
             if (read_char != '\0')
             {
+                ESP_LOGI(TAG, "read_char = '%c'", read_char);
                 // Write data read from terminal to Slave
                 uart_write_bytes(UART_NUM_2, (const char *) &read_char, 1);
                 read_char = '\0';
